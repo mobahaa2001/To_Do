fc6bdb7c29d9e51e9f47ab2767de326d6504d957
Mego <70282836+mobahaa2001@users.noreply.github.com>
2023-03-01 10:36:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
-
+$todos = [];
+if (file_exists('todo.json')) {
+    $json = file_get_contents('todo.json');
+    $todos = json_decode($json, true);
+}
 ?>
 
 
@@ -19,6 +23,30 @@
         <input type="text" name="todo_name" placeholder="Enter Your ToDo">
         <button>New ToDo</button>
     </form>
+    <br>
+
+    <?php foreach ($todos as $todoName => $todo) : ?>
+        <div style="margin-bottom:20px;">
+            <form style="display: inline-block" action="change_status.php" method="post">
+                <input type="hidden" name="todo_name" value="<?php echo $todoName ?>">
+                <input type="checkbox" <?php $todo['completed'] ? 'checked' : '' ?>>
+            </form>
+            <?php echo $todoName; ?>
+            <form style="display: inline-block" action="delete.php" method="post">
+                <input type="hidden" name="todo_name" value="<?php echo $todoName ?>">
+                <button>Delete</button>
+            </form>
+        </div>
+    <?php endforeach; ?>
+
+    <script>
+        const checkboxes = document.querySelectorAll('input[type= checkbox][name=todo_name]');
+        checkboxes.forEach(x => {
+            x.onClick = function() {
+                this.parentNode.submit();
+            };
+        })
+    </script>
 </body>
 
 </html>
