2cfe451964df50e9cf81d311ff5d60853bf25840
Mego <70282836+mobahaa2001@users.noreply.github.com>
2023-03-01 10:37:08 +0200

    Update newToDo.php

--- newToDo.php
+++ newToDo.php
@@ -8,13 +8,16 @@ $todoName = $_POST['todo_name'] ?? false;
 $todoName = trim($todoName);
 
 if ($todoName) {
-    $json = file_get_contents('todo.json');
-    $jsonArray = json_decode($json);
+    if (file_exists('todo.json')) {
+        $json = file_get_contents('todo.json');
+        $jsonArray = json_decode($json, true);
+    } else {
+        $jsonArray = [];
+    }
     $jsonArray[$todoName] = ['completed' => false];
-    // echo '<pre>';
-    // var_dump($jsonArray);
-    // echo '<pre>';
-    // 
+
     // echo $json;
-    file_put_contents('todo.json', json_encode($jsonArray));
+    file_put_contents('todo.json', json_encode($jsonArray, JSON_PRETTY_PRINT));
 }
+
+header('location: index.php');
